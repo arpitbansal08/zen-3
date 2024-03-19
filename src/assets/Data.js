557a568c8b2e0e2827f87abbb056c87d051a605e
arpitbansal08 <101734269+arpitbansal08@users.noreply.github.com>
2024-03-19 20:30:14 +0530

    Added graphs using graphjs-2

---
+++ src/assets/Data.js
@@ -0,0 +1,65 @@
+export const revenueData = [
+  {
+    id: 1,
+    revenue: "16000",
+    city: "Social Circle",
+  },
+  {
+    id: 2,
+    revenue: "13000",
+    city: "Malibu",
+  },
+  {
+    id: 3,
+    revenue: "6200",
+    city: "Snohomish",
+  },
+  {
+    id: 4,
+    revenue: "5900",
+    city: "New York",
+  },
+  {
+    id: 5,
+    revenue: "2800",
+    city: "Everett",
+  },
+  {
+    id: 6,
+    revenue: "2200",
+    city: "Seattle",
+  },
+  {
+    id: 7,
+    revenue: "1800",
+    city: "London",
+  },
+];
+
+export const revenueByJobType = [
+  {
+    id: 1,
+    job:"alarm System",
+    revenue:31000,
+  },
+  {
+    id: 2,
+    job: "BackFlow Testing",
+    revenue: 17500,
+  },
+  {
+    id: 3,
+    job: "Diagnosis",
+    revenue: 6900,
+  },
+  {
+    id: 4,
+    job: "Fire Extinguisher",
+    revenue: 5900,
+  },
+  {
+    id: 5,
+    job: "Pump",
+    revenue: 1500,
+  },
+];
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-
+import { revenueData } from "../assets/Data.js";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import { useState } from "react";
+import { revenueByJobType } from "../assets/Data.js";
 const MetricCard = ({ value, title }) => {
   return (
-    <div className="bg-white h-20 w-48 p-2 flex flex-col justify-between border-l-red-400 border-2">
+    <div className="item bg-white h-20 w-48 p-2 flex flex-col justify-between border-l-red-400 border-2">
       <div>{value}</div>
       <div className="align-bottom">{title}</div>
     </div>
@@ -21,6 +25,36 @@ const Dashboard = () => {
     { value: "2", title: "Total Canceled" },
   ];
 
+  const [revenue, setRevenue] = useState({
+    labels: revenueData.map((data) => {
+      return data.city;
+    }),
+
+    datasets: [
+      {
+        label: "Revenue for December 2023",
+        axis: "y",
+        data: revenueData.map((data) => {
+          return data.revenue;
+        }),
+      },
+    ],
+  });
+  const [revenueJob, setRevenueJob] = useState({
+    labels: revenueByJobType.map((data) => {
+      return data.job;
+    }),
+    datasets: [
+      {
+        label: "Revenue by Job Type",
+        axis: "y",
+        data: revenueByJobType.map((data) => {
+          return data.revenue;
+        }),
+      },
+    ],
+  });
+
   return (
     <div className="m-8 p-8 bg-slate-200 h-screen w-80vh">
       <h3>Company Metrics</h3>
@@ -34,7 +68,52 @@ const Dashboard = () => {
           />
         ))}
       </div>
-
+      <div className="grid grid-cols-2 ">
+        <div className="item border-2 border-black m-1 p-2">
+          <h4>Revenue By Job Location</h4>
+          <Bar
+            data={revenue}
+            options={{
+              indexAxis: "y",
+              backgroundColor: "rgba(248,113,113,1)",
+              scales: {
+                x: {
+                  grid: {
+                    display: false,
+                  },
+                },
+                y: {
+                  grid: {
+                    display: false,
+                  },
+                },
+              },
+            }}
+          />
+        </div>
+        <div className="item border-2 border-black m-1 p-2">
+          <h4>Revenue by Job Type</h4>
+          <Bar
+            data={revenueJob}
+            options={{
+              indexAxis: "y",
+              backgroundColor: "rgba(248,113,113,1)",
+              scales: {
+                x: {
+                  grid: {
+                    display: false,
+                  },
+                },
+                y: {
+                  grid: {
+                    display: false,
+                  },
+                },
+              },
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
